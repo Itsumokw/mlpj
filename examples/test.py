f3075a72fde93a6e24b21e76ffe601943804aaa3
Lide-Ren <t202410246994787@eduxiji.net>
2025-06-18 21:24:42 +0800

    编写了requirements.txt，并删除了一些没用的文件。

--- examples/test.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("mnassrib/jena-climate")
-
-print("Path to dataset files:", path)
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# FILE: /ts-ml-framework/ts-ml-framework/tests/__init__.py
-
-# This file is intentionally left blank.
